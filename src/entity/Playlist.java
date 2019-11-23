96f2184d481060a239a0bfb753c9854d8f109e65
MarkForte <mjforte@wpi.edu>
2019-11-23 16:15:06 -0500

    Added library function skeletons

--- src/entity/Playlist.java
+++ src/entity/Playlist.java
@@ -1,12 +1,27 @@
 package entity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Playlist {
-	ArrayList<VideoSegment> videos;
+	final List<VideoSegment> videos;
 	String name;
 	
 	public Playlist (String name) {
 		this.name = name;
+		videos = new ArrayList<VideoSegment>();
 	}
+	
+	boolean addSegment(VideoSegment segment) {
+		return false;
+	}
+	
+	boolean removeSegment(VideoSegment segment) {
+		return false;
+	}
+	
+	boolean rename(String name) {
+		return false;
+	}
+	
 }
