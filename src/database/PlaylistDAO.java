4785b4a350cc894bcbc5840ebff18d2c5e329449
as <ajsmith2@wpi.edu>
2019-11-25 18:16:32 -0500

    Finished PlaylistDAO

--- src/database/PlaylistDAO.java
+++ src/database/PlaylistDAO.java
@@ -90,14 +90,91 @@ public class PlaylistDAO {
 		}
 	}
 	
-	//TODO
-	public boolean appendVideoToPlaylist (String name, VideoSegment vs) {
-		return false;
+	/**
+	 * Appends a video to the playlist in the RDS.
+	 * @param name The name of the playlist.
+	 * @param videoLocation the Location of the video in the S3 bucket.
+	 * @return True if appended. False otherwise.
+	 */
+	public boolean appendVideoToPlaylist (String name, String videoLocation) {
+		try {
+			String id = getID(name);
+			int order = getNumVideos(id);
+			pstmt = conn.prepareStatement("INSERT INTO Playlist (playlistID, videoLocation, videoOrder) VALUES (?, ?, ?)");
+			pstmt.setString(1, id);
+			pstmt.setString(2, videoLocation);
+			pstmt.setInt(3, order + 1);
+			int numRows = pstmt.executeUpdate();
+			return (numRows == 1);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
-	//TODO
-	public boolean removeVideoFromPlaylist (String name, String videoLocation) {
-		return false;
+	/**
+	 * Private helper method to get the ID of a playlist from the name.
+	 * @param name The name of a playlist.
+	 * @return The ID.
+	 * @throws SQLException
+	 */
+	private String getID (String name) throws SQLException {
+		try {
+			pstmt = conn.prepareStatement("SELECT playlistID FROM Library WHERE playlistName = ?");
+			pstmt.setString(1, name);
+			rset = pstmt.executeQuery();
+			rset.next();
+			String id = rset.getString("playlistID");
+			return id;
+		} finally {
+			closeStmtRset();
+		}
+	}
+	
+	/**
+	 * Gets the number of videos currently in a playlist.
+	 * @param id The ID of the playlist.
+	 * @return The number of videos in the playlist.
+	 * @throws SQLException
+	 */
+	private int getNumVideos(String id) throws SQLException {
+		try {
+			pstmt = conn.prepareStatement("SELECT COUNT(videoOrder) AS num FROM Playlist WHERE playlistID = ?");
+			pstmt.setString(1, id);
+			rset = pstmt.executeQuery();
+			rset.next();
+			int count = rset.getInt("num");
+			return count;
+		} finally {
+			closeStmtRset();
+		}
+	}
+	
+	/**
+	 * Deletes the video at the specified spot in the playlist. Order starts at 1.
+	 * @param name The name of the playlist.
+	 * @param order The index of the Video.
+	 * @return True if deleted. False otherwise.
+	 */
+	public boolean removeVideoFromPlaylist (String name, int order) {
+		try {
+			String id = getID(name);
+			pstmt = conn.prepareStatement("DELETE FROM Playlist WHERE playlistID = ?, videoOrder = ?");
+			pstmt.setString(1, id);
+			pstmt.setInt(2, order);
+			pstmt.executeUpdate();
+			pstmt.close();
+			pstmt = conn.prepareStatement("UPDATE FROM Playlist SET videoOrder = videoOrder - 1 WHERE playlistID = ?, videoOrder > ?");
+			pstmt.setString(1, id);
+			pstmt.setInt(2, order);
+			pstmt.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			closeStmtRset();
+		}
 	}
 	
 	/**
@@ -107,14 +184,7 @@ public class PlaylistDAO {
 	public List<Playlist> listAllPlaylists () {
 		try {
 			List<Playlist> p = new ArrayList<Playlist>();
-			pstmt = conn.prepareStatement("SELECT playlistName FROM Library");
-			rset = pstmt.executeQuery();
-			List<String> names = new ArrayList<String>();
-			while (rset.next()) {
-				names.add(rset.getString("playlistName"));
-			}
-			pstmt.close();
-			rset.close();
+			List<String> names = listAllPlaylistNames();
 			for (String name : names) {
 				p.add(getPlaylist(name));
 			}
@@ -127,9 +197,25 @@ public class PlaylistDAO {
 		}
 	}
 	
-	//TODO
-	public List<Playlist> listAllPlaylistNames () {
-		return null;
+	/**
+	 * Gets all of the names of each playlist from the database.
+	 * @return A list of all the names.
+	 */
+	public List<String> listAllPlaylistNames () {
+		try {
+			pstmt = conn.prepareStatement("SELECT playlistName FROM Library");
+			rset = pstmt.executeQuery();
+			List<String> names = new ArrayList<String>();
+			while (rset.next()) {
+				names.add(rset.getString("playlistName"));
+			}
+			return names;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			closeStmtRset();
+		}
 	}
 	
 	/**
