88a490af3033f1f07ffb0109bd560bdc1fca877b
MarkForte <mjforte@wpi.edu>
2019-12-04 14:19:23 -0500

    Added Lambda Test

---
+++ src/test/java/lambdas/LambdaTest.java
@@ -0,0 +1,25 @@
+package lambdas;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+public abstract class LambdaTest {
+	
+	/**
+	 * Helper method that creates a context that supports logging so you can test lambda functions
+	 * in JUnit without worrying about the logger anymore.
+	 * 
+	 * @param apiCall      An arbitrary string to identify which API is being called.
+	 * @return
+	 */
+	Context createContext(String apiCall) {
+        TestContext ctx = new TestContext();
+        ctx.setFunctionName(apiCall);
+        return ctx;
+    }
+}
