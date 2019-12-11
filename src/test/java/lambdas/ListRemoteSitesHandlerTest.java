2bdbdd1bb1e27673f57529d88312630b8628d1dd
Benjamin Babalola <benjamin111223@yahoo.com>
2019-12-11 13:52:28 -0500

    Tests for DeleteVideoSegment, FlipMark, and ListRemoteSites Handlers

---
+++ src/test/java/lambdas/ListRemoteSitesHandlerTest.java
@@ -0,0 +1,50 @@
+package lambdas;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+import database.PlaylistDAO;
+import database.RemoteSiteDAO;
+import entity.Playlist;
+import http.ListPlaylistResponse;
+import http.ListRemoteSitesResponse;
+
+public class ListRemoteSitesHandlerTest extends LambdaTest {
+
+	RemoteSiteDAO dao;
+	String testSite;
+	
+	@Before
+	public void setUp() throws Exception {
+		dao = new RemoteSiteDAO();
+		testSite = "google.com";
+		dao.addRemoteSite(testSite);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		dao.removeRemoteSite(testSite);
+		dao.close();
+	}
+
+	@Test
+	public void test() {
+		ListRemoteSitesHandler handler = new ListRemoteSitesHandler();
+		Context context = createContext("listRemoteSites");
+		ListRemoteSitesResponse response = handler.handleRequest(null, context);
+		assertEquals(response.statusCode, 200);
+		boolean containsTest = false;
+		for(String RS : response.list) {
+			if(RS.equals(testSite)) {
+				containsTest = true;
+			}
+		}
+		assertTrue(containsTest);
+	}
+
+}
